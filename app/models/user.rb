9f4edaaf0eaa06aa62a77756c0936f28e521c2e5
Wesley <matthew282@msn.com>
2022-07-13 20:12:45 -0700

    added username verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ has_person_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
+
+  validates_uniqueness of :username
 end
