e1d7092f0486456e4d8740b7ba90303ec0ddc957
Wesley <matthew282@msn.com>
2022-07-21 11:15:50 -0700

    create migrations for retweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_201833) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_181414) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -143,6 +143,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_201833) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "tweet_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
